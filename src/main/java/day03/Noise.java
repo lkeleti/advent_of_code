37ff4390a1ea16c46d6cedfe3d7c316897e91a44
lkeleti <keleti.laszlo@vipmail.hu>
2021-12-03 10:24:51 +0100

    day03 exercise 2 completed

--- src/main/java/day03/Noise.java
+++ src/main/java/day03/Noise.java
@@ -9,5 +9,11 @@ public class Noise {
     public static void main(String[] args) {
         NoiseController noiseController = new NoiseController();
         System.out.println(noiseController.readData(Path.of("src/main/resources/noiseinput.txt")));
+
+        int oxygen = noiseController.SecondExerciseA(Path.of("src/main/resources/noiseinput.txt"));
+        int co2 = noiseController.SecondExerciseB(Path.of("src/main/resources/noiseinput.txt"));
+
+        System.out.println(oxygen*co2);
+
     }
 }
--- src/main/java/day03/NoiseController.java
+++ src/main/java/day03/NoiseController.java
@@ -3,10 +3,89 @@ package day03;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NoiseController {
     private int[] bits = new int[12];
+
+    public int SecondExerciseA(Path path) {
+        List<String> lines = readFile(path);
+        for (int i =  0 ; i < lines.get(0).length(); i++) {
+            lines = findOne(lines, i);
+        }
+        System.out.println(lines);
+        return Integer.parseInt(lines.get(0),2);
+    }
+
+    public int SecondExerciseB(Path path) {
+        List<String> lines = readFile(path);
+        for (int i = 0; i < lines.get(0).length(); i++) {
+            lines = findZeros(lines, i);
+        }
+        System.out.println(lines);
+        return Integer.parseInt(lines.get(0),2);
+    }
+
+    private List<String> findOne(List<String> numbers, int position) {
+        List<String> result = new ArrayList<>();
+        int ones = 0;
+        int zeros = 0;
+        for (String line: numbers) {
+            if (line.charAt(position) == '1') {
+                ones++;
+            }
+            else {
+                zeros++;
+            }
+        }
+
+        for (String line: numbers) {
+            if (ones >= zeros) {
+                if (line.charAt(position) == '1') {
+                    result.add(line);
+                }
+            }
+            else {
+                if (line.charAt(position) == '0') {
+                    result.add(line);
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<String> findZeros(List<String> numbers, int position) {
+        List<String> result = new ArrayList<>();
+        int ones = 0;
+        int zeros = 0;
+        for (String line: numbers) {
+            if (line.charAt(position) == '1') {
+                ones++;
+            }
+            else {
+                zeros++;
+            }
+        }
+
+
+        for (String line: numbers) {
+            if (zeros <= ones) {
+                if (line.charAt(position) == '0') {
+                    result.add(line);
+                }
+            }
+            else {
+                if (line.charAt(position) == '1') {
+                    result.add(line);
+                }
+            }
+        }
+        if (zeros + ones == 1 && result.size() == 0) {
+            result.add(numbers.get(0));
+        }
+        return result;
+    }
 
     public int readData(Path path) {
         List<String> lines = readFile(path);
