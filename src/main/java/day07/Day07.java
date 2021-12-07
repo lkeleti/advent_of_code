1bd19c46b1a94b930ed248e2826a97c7b0389c78
lkeleti <keleti.laszlo@vipmail.hu>
2021-12-07 09:51:54 +0100

    day07 exercise 2 completed

--- src/main/java/day07/Day07.java
+++ src/main/java/day07/Day07.java
@@ -3,6 +3,7 @@ package day07;
 public class Day07 {
     public static void main(String[] args) {
         CrabController crabController = new CrabController();
-        System.out.println(crabController.guessSmallestCost());
+        System.out.println(crabController.guessSmallestCost(true));
+        System.out.println(crabController.guessSmallestCost(false));
     }
 }
